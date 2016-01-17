c57b1762bd8c56b1e8968b7aa996fce40784c829
Eddie Parker <Eddie Parker>
2016-01-17 14:04:00 +0000

    Changed control system to send radio messages depending on size of error

--- ArduinoUno/SwimSpeedController/SwimSpeedController.ino
+++ ArduinoUno/SwimSpeedController/SwimSpeedController.ino
@@ -4,14 +4,9 @@
 static const unsigned int RADIO_CYCLE_TIME = 350; /* Microsecs */
 static const unsigned int RADIO_SHORT_CYCLE_COUNT = 1; /* 1x the CYCLE TIME */
 static const unsigned int RADIO_LONG_CYCLE_COUNT = 3; /* 3x the CYCLE TIME */
-static const unsigned long DELAY_BETWEEN_MESSAGES = 100; /* Milliseconds */
-static const unsigned long SHORT_DELAY_BETWEEN_MESSAGE_BURSTS = 1000; /* Milliseconds */
-static const unsigned long LONG_DELAY_BETWEEN_MESSAGE_BURSTS = 5000; /* Milliseconds */
-static const unsigned long VERY_LONG_DELAY_BETWEEN_MESSAGE_BURSTS = 10000; /* Milliseconds */
-static const unsigned long MESSAGE_BURST_COUNT = 4;
+static const unsigned long MIN_DELAY_BETWEEN_MESSAGES = 100; /* Milliseconds */
 static const char *slower =  "0111100010100001011010000101";
 static const char *faster =  "0111100010011001011010010101";
-static const char *onoff =   "0111100010001001011010000101";
 
 struct RADIO_BIT
 {
@@ -24,16 +19,14 @@ static const RADIO_BIT offBIT = {RADIO_SHORT_CYCLE_COUNT, RADIO_LONG_CYCLE_COUNT
 
 static RADIO_BIT slower_bits[ 28 ];  
 static RADIO_BIT faster_bits[ 28 ];
-static RADIO_BIT onoff_bits[ 28 ];
 
 static RADIO_BIT* next_radio_bit = NULL;
 static int radio_bit_countdown = 0;
 static int radio_pin_state = LOW;
 static const int radio_pin = 10;
 
-static long radioTX_interval = DELAY_BETWEEN_MESSAGES;
+static long radioTX_interval = MIN_DELAY_BETWEEN_MESSAGES;
 static unsigned long radioTX_previousMillis = 0;
-static int radioTX_burst_counter = MESSAGE_BURST_COUNT;
 
 static const long sample_interval = 10;
 static unsigned long sample_previousMillis = 0;
@@ -107,19 +100,9 @@ void setup()
     {
       faster_bits[ i ] = offBIT;
     }
-    if (onoff[ i ] == '1')
-    {
-      onoff_bits[ i ] = onBIT;
-    }
-    else
-    {
-      onoff_bits[ i ] = offBIT;
-    }
   }
 }
 
-int sample_count = 0;
-int radio_message_count = 0;
 static word command_register = 0;
 static word response_register = 0;
 static word speed_demand = 0;
@@ -127,13 +110,11 @@ static word speed_actual = 0;
 static const byte START_STOP_BIT = 0;
 static const byte CONTROL_BIT = 1;
 
-static bool start_stop_command = false;
-static bool start_stop_response = false;
 static bool control_command = false;
 static bool control_response = false;
 
 static word control_deadband = 10;
-static word control_slowband = 20;
+static word control_timeconst = 10000;
 
 static word A0_filter_coeff = 32000;
 static float A0_filter_value = 0;
@@ -185,9 +166,6 @@ void loop()
     analogue_sample *= (0x8000 - A1_filter_coeff);
     A1_filter_value = (analogue_sample + (A1_filter_value * A1_filter_coeff)) / 0x8000;
     supply_voltage = A1_filter_value;
-
-                    
-    sample_count++;    
   }
 
   /*
@@ -198,24 +176,40 @@ void loop()
   /*
   * Handle the modbus comms
   */
-  setBitState(&response_register, START_STOP_BIT, start_stop_response);
   setBitState(&response_register, CONTROL_BIT, control_response);
   mbs.registers[ 1 ] = response_register;  
   mbs.registers[ 3 ] = speed_actual;
   mbs.registers[ 4 ] = control_deadband;
-  mbs.registers[ 5 ] = control_slowband;
+  mbs.registers[ 5 ] = control_timeconst;
   mbs.registers[ 6 ] = A0_filter_coeff;
   mbs.registers[ 7 ] = supply_voltage;
   mbs.registers[ 8 ] = A1_filter_coeff;
+  mbs.registers[ 9 ] = radioTX_interval & 0xFFFF;
   mbs.task();
   command_register = mbs.registers[ 0 ];
   speed_demand = mbs.registers[ 2 ];
   control_deadband = mbs.registers[ 4 ];
-  control_slowband = mbs.registers[ 5 ];
+  control_timeconst = mbs.registers[ 5 ];
   A0_filter_coeff = min(mbs.registers[ 6 ], 0x7FFF);    
   A1_filter_coeff = min(mbs.registers[ 8 ], 0x7FFF);    
-  start_stop_command = getBitState(&command_register, START_STOP_BIT);
   control_command = getBitState(&command_register, CONTROL_BIT);
+    
+  /*
+  * Calculate the radioTX_interval based on control error
+  */
+  word control_error = abs(speed_actual - speed_demand);
+  if (control_error > control_deadband)
+  {
+    radioTX_interval = control_timeconst / (control_error * control_error);
+    radioTX_interval = max(radioTX_interval, MIN_DELAY_BETWEEN_MESSAGES);
+  }
+  else
+  {
+    // Set the interval to max
+    radioTX_interval = 0xFFFF;
+    // Act as if we sent a message as the speed is correct
+    radioTX_previousMillis = currentMillis;
+  }
     
   /*
   * Time for Radio Transmission
@@ -227,101 +221,21 @@ void loop()
     /* Only try sending if not already */
     if (next_radio_bit == NULL)
     {
-      if (start_stop_command != start_stop_response)
+      if (control_command)
       {
-        next_radio_bit = onoff_bits;
-        radio_bit_countdown = 28;
-        radioTX_interval = SHORT_DELAY_BETWEEN_MESSAGE_BURSTS;     
-      }
-      else if (control_command)
-      {
-        if (speed_demand > (speed_actual + control_deadband))
+        if (speed_demand > speed_actual)
         {
           next_radio_bit = faster_bits;
           radio_bit_countdown = 28;
-          if (speed_demand > (speed_actual + control_slowband))
-          {
-            radioTX_interval = SHORT_DELAY_BETWEEN_MESSAGE_BURSTS;        
-          }
-          else
-          {
-            radioTX_interval = LONG_DELAY_BETWEEN_MESSAGE_BURSTS;        
-          }
         }
-        else if (speed_demand < (speed_actual - control_deadband))
+        else if (speed_demand < speed_actual)
         {
           next_radio_bit = slower_bits;
           radio_bit_countdown = 28;
-          if (speed_actual > (speed_demand + control_slowband))
-          {
-            radioTX_interval = SHORT_DELAY_BETWEEN_MESSAGE_BURSTS;        
-          }
-          else
-          {
-            radioTX_interval = LONG_DELAY_BETWEEN_MESSAGE_BURSTS;        
-          }        
         }
       }
-      
-      radioTX_burst_counter--;
-      if (radioTX_burst_counter > 0)
-      {
-        radioTX_interval = DELAY_BETWEEN_MESSAGES;
-      }
-      else
-      {
-        radioTX_burst_counter = MESSAGE_BURST_COUNT;
-        start_stop_response = start_stop_command;
-      }
     }
-    else
-    {
-      radioTX_interval = DELAY_BETWEEN_MESSAGES;
-    } 
   }
-  
-  
-  /*
-  * Time for Serial Transmission
-  */
-  /*if(currentMillis - serialTX_previousMillis >= serialTX_interval) 
-  {
-    // save the last time
-    serialTX_previousMillis = currentMillis;
-    TX_buffer[ 1 ] = ss_status;
-    TX_buffer[ 2 ] = control_status;
-    int_to_chars(speed_demand, TX_buffer + 4);
-    int_to_chars(speed_actual, TX_buffer + 8);
-    cs = calc_checksum(TX_buffer, 12);
-    int_to_chars(cs, TX_buffer + 12);
-    Serial.print(TX_buffer);
-  }*/
-    
-  // print the string when a newline arrives:
-  /*if (stringComplete) 
-  {
-    if (inputString.length() == 12)
-    {
-      inputString.toCharArray(RX_buffer, 12);
-      cs = calc_checksum(RX_buffer, 8);
-      byte cs2 = atoi(RX_buffer + 8);
-      if (cs != cs2)
-      {
-        Serial.print("ERROR : Checksum should be ");
-        Serial.println(cs);
-      }
-      else
-      {
-        ss_command = RX_buffer[ 1 ];
-        control_command = RX_buffer[ 2 ];
-        control_status = control_command;
-        speed_demand = atoi(RX_buffer + 4);
-      }
-    } 
-    // Always clear the string 
-    inputString = "";
-    stringComplete = false; 
-  }*/
 }
 
 
