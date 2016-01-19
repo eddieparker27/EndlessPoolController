cc3c0af827dba653dfa0db91c85f08c5d6341de4
Eddie Parker <eddieparker27@gmail.com>
2016-01-19 04:34:45 +0000

    Testing CRC faults

--- RaspberryPi/NodeWebServer/modbus4.js
+++ RaspberryPi/NodeWebServer/modbus4.js
@@ -63,6 +63,15 @@ function write_modbus_queue(req_function)
    modbus_queue_len++;
 }
 
+/*
+* Initialise Variables 
+*/
+setValue("speed_actual_filter_COF", system_state.speed_actual_filter_COF);
+setValue("supply_power_filter_COF", system_state.supply_power_filter_COF);
+setValue("control_deadband_volts", system_state.control_deadband_volts);
+setValue("control_timeconst", system_state.control_timeconst);
+
+
 setInterval(read_registers, 1000);
 
 function read_registers()
@@ -81,8 +90,8 @@ function read_registers()
          system_state.speed_actual_register = holding_registers[HOLDING_REGISTER_ADDRESS.speed_actual];
          system_state.speed_demand_volts = system_state.speed_demand_register * 5.0 / 1024;
          system_state.speed_actual_volts = system_state.speed_actual_register * 5.0 / 1024;
-         system_state.speed_demand = Math.round((Math.min(Math.max(system_state.speed_demand_volts, 1.25), 3.05) - 1.25) * 30) + 1;
-         system_state.speed_actual = Math.round((Math.min(Math.max(system_state.speed_actual_volts, 1.25), 3.05) - 1.25) * 30) + 1;
+         system_state.speed_demand = ((Math.min(Math.max(system_state.speed_demand_volts, 1.25), 3.05) - 1.25) * 30) + 1;
+         system_state.speed_actual = ((Math.min(Math.max(system_state.speed_actual_volts, 1.25), 3.05) - 1.25) * 30) + 1;
          system_state.control_deadband_volts = holding_registers[HOLDING_REGISTER_ADDRESS.control_deadband] * 5.0 / 1024;
          system_state.control_timeconst = holding_registers[HOLDING_REGISTER_ADDRESS.control_timeconst];
          system_state.speed_actual_filter_COF = Math.log(holding_registers[ HOLDING_REGISTER_ADDRESS.speed_actual_filter_COF ] / 32768.0) * 100 / (-2.0 * Math.PI);
@@ -95,6 +104,127 @@ function read_registers()
 //console.log(holding_registers);
      });
    });
+}
+
+
+function setValue(name, value)
+{
+    var success = false;
+    var address = 0;
+    
+    if (name === "speed_demand_register")
+    {
+        address = HOLDING_REGISTER_ADDRESS.speed_demand;
+        if ((value) && (value >= 0) && (value <= 1023))
+        {
+            value = Number(value);
+            success = true;
+        }
+    }
+    else if (name === "speed_demand_volts")
+    {
+        address = HOLDING_REGISTER_ADDRESS.speed_demand;
+        if ((value) && (value >= 0.0) && (value <= 5.0))
+        {
+            value *= 1024.0 / 5.0;
+            value = Math.round(value);
+            value = Math.max(value, 0);
+            value = Math.min(value, 1023);
+            success = true;
+        }
+    }
+    else if (name === "speed_demand")
+    {
+        address = HOLDING_REGISTER_ADDRESS.speed_demand;
+        if ((value) && (value >= 1) && (value <= 55))
+        {
+            value--;
+            value /= 30;
+            value += 1.25;
+            value *= 1024.0 / 5.0;
+            value = Math.round(value);
+            value = Math.max(value, 0);
+            value = Math.min(value, 1023);
+            success = true;
+        }
+    }
+    else if (name === "speed_actual_filter_COF")
+    {
+        address = HOLDING_REGISTER_ADDRESS.speed_actual_filter_COF;
+        if ((value) && (value >= 0.0) && (value <= 50.0))
+        {
+            value *= -2.0 * Math.PI / 100.0;
+            value = Math.exp(value) * 32768.0;
+            value = Math.round(value);
+            value = Math.max(0, value);
+            value = Math.min(0x8000, value);
+            success = true;
+        }
+    }
+    else if (name === "supply_power_filter_COF")
+    {
+        address = HOLDING_REGISTER_ADDRESS.supply_power_filter_COF;
+        if ((value) && (value >= 0.0) && (value <= 50.0))
+        {
+            value *= -2.0 * Math.PI / 100.0;
+            value = Math.exp(value) * 32768.0;
+            value = Math.round(value);
+            value = Math.max(0, value);
+            value = Math.min(0x8000, value);
+            success = true;
+        }
+    }
+    else if (name === "control_active")
+    {
+        address = HOLDING_REGISTER_ADDRESS.control;
+        is_active = value;
+        var value = holding_registers[address];
+        value &= ~(CONTROL_ACTIVE_BIT);
+
+        if (typeof is_active === "boolean")
+        {
+            if (is_active)
+            {
+                value |= CONTROL_ACTIVE_BIT;
+            }
+            success = true;
+        }
+    }
+    else if (name === "control_deadband_volts")
+    {
+        address = HOLDING_REGISTER_ADDRESS.control_deadband;
+        if ((value) && (value >= 0.0) && (value <= 5.0))
+        {
+            value *= 1024.0 / 5.0;
+            value = Math.round(value);
+            value = Math.max(value, 0);
+            value = Math.min(value, 1023);
+            success = true;
+        }
+    }
+    else if (name === "control_timeconst")
+    {
+        address = HOLDING_REGISTER_ADDRESS.control_timeconst;
+        if ((value) && (value >= 0.0) && (value <= 0xFFFF))
+        {
+            value = Number(value);
+            success = true;
+        }
+    }
+
+    if (success)
+    {
+        write_modbus_queue(function ()
+        {
+            client.request(FC.WRITE_SINGLE_REGISTER, address, value, function (err, MB_response)
+            {
+                modbus_busy = false;
+                if (err) throw err;
+                console.log(MB_response);
+            });
+        });
+    }
+    return success;
 }
 
 
@@ -119,124 +249,31 @@ http.createServer(function(request, response) {
     
      request.on('end', function() 
         {
-           var success = false;
-           var address = 0;
-           var value = 0;
-           if (uri === "/system_state/speed_demand_register") {
-               address = HOLDING_REGISTER_ADDRESS.speed_demand;
-               value = JSON.parse(body);
-               if ((value) && (value >= 0) && (value <= 1023)) {
-                   value = Number(value);
-                   success = true;
-               }
-           }
-           else if (uri === "/system_state/speed_demand_volts")
-           {              
-              address = HOLDING_REGISTER_ADDRESS.speed_demand;
-              value = JSON.parse(body);
-              if ((value) && (value >= 0.0) && (value <= 5.0))
-              {
-                 value *= 1024.0 / 5.0;
-                 value = Math.round(value);
-                 value = Math.max(value, 0);
-                 value = Math.min(value, 1023);
-                 success = true;
-              }
-           }
-           else if (uri === "/system_state/speed_demand") {
-               address = HOLDING_REGISTER_ADDRESS.speed_demand;
-               value = JSON.parse(body);
-               if ((value) && (value >= 1) && (value <= 55)) {                   
-                   value--;
-                   value /= 30;
-                   value += 1.25;
-                   value *= 1024.0 / 5.0;
-                   value = Math.round(value);
-                   value = Math.max(value, 0);
-                   value = Math.min(value, 1023);
-                   success = true;
-               }
-           }
-           else if (uri === "/system_state/speed_actual_filter_COF")
-           {
-              address = HOLDING_REGISTER_ADDRESS.speed_actual_filter_COF;
-              value = JSON.parse(body);
-              if ((value) && (value >= 0.0) && (value <= 50.0))
-              {
-                 value *= -2.0 * Math.PI / 100.0;
-                 value = Math.exp(value) * 32768.0;
-                 value = Math.round(value);
-                 value = Math.max(0, value);
-                 value = Math.min(0x8000, value);
-                 success = true;
-              }
-           }
-           else if (uri === "/system_state/supply_power_filter_COF") {
-               address = HOLDING_REGISTER_ADDRESS.supply_power_filter_COF;
-               value = JSON.parse(body);
-               if ((value) && (value >= 0.0) && (value <= 50.0)) {
-                   value *= -2.0 * Math.PI / 100.0;
-                   value = Math.exp(value) * 32768.0;
-                   value = Math.round(value);
-                   value = Math.max(0, value);
-                   value = Math.min(0x8000, value);
-                   success = true;
-               }
-           }
-           else if (uri === "/system_state/control_active") {
-               address = HOLDING_REGISTER_ADDRESS.control;               
-               is_active = JSON.parse(body);
-               var value = holding_registers[address];
-               value &= ~(CONTROL_ACTIVE_BIT);
-
-               if (typeof is_active === "boolean")
+           if ((uri === "/system_state/speed_demand_register") ||
+               (uri === "/system_state/speed_demand_volts") ||
+               (uri === "/system_state/speed_demand") ||
+               (uri === "/system_state/speed_actual_filter_COF") ||
+               (uri === "/system_state/supply_power_filter_COF") ||
+               (uri === "/system_state/control_active") ||
+               (uri === "/system_state/control_deadband_volts") ||
+               (uri === "/system_state/control_timeconst"))
                {
-                   if (is_active)
-                   {
-                       value |= CONTROL_ACTIVE_BIT;
-                   }                   
-                   success = true;
-               }
-           }
-           else if (uri === "/system_state/control_deadband_volts") {
-               address = HOLDING_REGISTER_ADDRESS.control_deadband;
-               value = JSON.parse(body);
-               if ((value) && (value >= 0.0) && (value <= 5.0)) {
-                   value *= 1024.0 / 5.0;
-                   value = Math.round(value);
-                   value = Math.max(value, 0);
-                   value = Math.min(value, 1023);
-                   success = true;
-               }
-           }
-           else if (uri === "/system_state/control_timeconst") {
-               address = HOLDING_REGISTER_ADDRESS.control_timeconst;
-               value = JSON.parse(body);
-               if ((value) && (value >= 0.0) && (value <= 0xFFFF)) {
-                   success = true;
-               }
-           }
+                  var name = uri.substr(14);
+                  value = JSON.parse(body);
 
-           if (success)
-           {
-              write_modbus_queue(function() {
-                 client.request(FC.WRITE_SINGLE_REGISTER, address, value, function(err, MB_response) {
-                    modbus_busy = false;
-                   if (err) throw err;
-                   console.log(MB_response);
-                   });
-                });
-
-              response.writeHead(204);
-              response.end();
-              return;
-           }
-           else
-           {
-              response.writeHead(400, {"Content-Type": "text/plain"});
-              response.write("400 Invalid Resourse [" + uri + "]\n");
-              response.end();
-              return;
+                  if (setValue(name, value))
+                  {
+                     response.writeHead(204);
+                     response.end();
+                     return;
+                  }
+                  else
+              {
+                 response.writeHead(400, { "Content-Type": "text/plain" });
+                 response.write("400 Invalid Resourse [" + uri + "]\n");
+                 response.end();
+                 return;
+              }
            }
         });
   }
